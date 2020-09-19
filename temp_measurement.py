ee127db8d4b999292f05db274c018963daeeb7b8
Catazza <cataldo.azzariti@gmail.com>
2020-09-19 19:10:08 +0100

    requests configured with token, seeing it on PS

--- temp_measurement.py
+++ temp_measurement.py
@@ -1,21 +1,35 @@
 import adafruit_dht
 import time
 import board
+import base64
+import requests
 from utils.jwt import create_jwt
 
 # --------- User Settings ---------
 SENSOR_LOCATION_NAME = "Main Living Room"
 SECONDS_BETWEEN_READS = 10
 METRIC_UNITS = False
-PROJECT_ID = 'temp-measure-dev'
+GCP_PROJECT_ID = 'temp-measure-dev'
 PRIVATE_KEY_FILE_PATH = './keys/rsa_private.pem'
 ENCRYPTION_ALGO = 'RS256' 
+CLOUD_REGION = 'europe-west1'
+REGISTRY_ID = 'temp-sensors'
+DEVICE_ID = 'rasp-pi-dht22'
 # ---------------------------------
 
 dhtSensor = adafruit_dht.DHT22(board.D4)
 
-jwt_token = create_jwt(PROJECT_ID, PRIVATE_KEY_FILE_PATH, ENCRYPTION_ALGO)
-print(jwt_token)
+# TODO: Add token refreshing logic
+jwt_token = create_jwt(GCP_PROJECT_ID, PRIVATE_KEY_FILE_PATH, ENCRYPTION_ALGO).decode('ascii')  # decode again as need the string representation for the request
+print("jwt_token: ", jwt_token, "\n")
+
+headers = {
+    'authorization': f'Bearer {jwt_token}',
+    'content-type': 'application/json',
+    'cache-control': 'no-cache'
+}
+url = f'https://cloudiotdevice.googleapis.com/v1/projects/{GCP_PROJECT_ID}/locations/{CLOUD_REGION}/registries/{REGISTRY_ID}/devices/{DEVICE_ID}:publishEvent'
+
 
 while True:
     try:
@@ -28,4 +42,9 @@ while True:
         humidity = "Error in measurement, skipping this beat"
     print(SENSOR_LOCATION_NAME + " Temperature(C)", temp_c)
     print(SENSOR_LOCATION_NAME + " Humidity(%)", humidity)
+
+    encoded_temp = base64.b64encode(f"temp is {temp_c} C".encode("ascii")).decode('ascii') # decode again as need the string representation for the request
+    payload = f'{{\"binary_data\": \"{encoded_temp}\"}}'
+    resp = requests.post(url, headers=headers,data=payload)
+    print(resp.text)
     time.sleep(SECONDS_BETWEEN_READS)
