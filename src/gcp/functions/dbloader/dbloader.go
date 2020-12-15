fbdfd57b969e38050a99b1ef244475e944c07d71
Cataldo Azzariti <cataldo.azzariti@sapient.com>
2020-12-15 20:46:10 +0000

    created tables on tf and also go client in cloud function

--- src/gcp/functions/dbloader/dbloader.go
+++ src/gcp/functions/dbloader/dbloader.go
@@ -3,8 +3,14 @@ package dbloader
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"os"
 	"time"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
 )
 
 // PubSubMessage is the payload of a Pub/Sub event.
@@ -14,9 +20,31 @@ type PubSubMessage struct {
 
 // TempMeasurement is the struct to Unmarshal the temperature measurement
 type TempMeasurement struct {
-	Temperature   string    `json:"temperature"`
-	Humidity      string    `json:"humidity"`
-	MeasuringTime time.Time `json:"measuring_time"`
+	Temperature            string    `json:"temperature"`
+	Humidity               string    `json:"humidity"`
+	MeasuringTime          time.Time `json:"measuring_time"`
+	DBLoaderProcessingTime time.Time
+}
+
+type StackOverflowRow struct {
+	URL       string `bigquery:"url"`
+	ViewCount int64  `bigquery:"view_count"`
+}
+
+// printResults prints results from a query to the Stack Overflow public dataset.
+func printResults(w io.Writer, iter *bigquery.RowIterator) error {
+	for {
+		var row StackOverflowRow
+		err := iter.Next(&row)
+		if err == iterator.Done {
+			return nil
+		}
+		if err != nil {
+			return fmt.Errorf("error iterating through results: %v", err)
+		}
+
+		fmt.Fprintf(w, "url: %s views: %d\n", row.URL, row.ViewCount)
+	}
 }
 
 // StoreTempMeasurementBQ consumes a Pub/Sub message.
@@ -24,6 +52,41 @@ func StoreTempMeasurementBQ(ctx context.Context, m PubSubMessage) error {
 	pubsubPayload := string(m.Data) // Automatically decoded from base64.
 	tempMeasurement := TempMeasurement{}
 	json.Unmarshal([]byte(pubsubPayload), &tempMeasurement)
-	log.Printf("Payload: %s!", pubsubPayload)
+	log.Printf("Payload: %s", pubsubPayload)
+
+	ctxWithTimeout, cancelFunction := context.WithTimeout(ctx, time.Duration(15)*time.Second)
+
+	client, err := bigquery.NewClient(ctxWithTimeout, "temp-measure-dev")
+	if err != nil {
+		log.Fatalf("bigquery.Newclient: %v", err)
+	}
+
+	defer func() {
+		fmt.Println("BQ Loader returned - canceling context")
+		client.Close()
+		cancelFunction()
+	}()
+
+	// example to check if reading works
+	query := client.Query(
+		`SELECT
+                CONCAT(
+                        'https://stackoverflow.com/questions/',
+                        CAST(id as STRING)) as url,
+                view_count
+        FROM ` + "`bigquery-public-data.stackoverflow.posts_questions`" + `
+        WHERE tags like '%google-bigquery%'
+        ORDER BY view_count DESC
+        LIMIT 10;`)
+
+	results, err := query.Read(ctxWithTimeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := printResults(os.Stdout, results); err != nil {
+		log.Fatal(err)
+	}
+
 	return nil
 }
--- src/gcp/functions/go.mod
+++ src/gcp/functions/go.mod
@@ -2,4 +2,8 @@ module azzadigital.com/tempmeasurement/cloudfunctions
 
 go 1.14
 
-require github.com/GoogleCloudPlatform/functions-framework-go v1.1.0
+require (
+	cloud.google.com/go/bigquery v1.8.0
+	github.com/GoogleCloudPlatform/functions-framework-go v1.1.0
+	google.golang.org/api v0.30.0
+)
