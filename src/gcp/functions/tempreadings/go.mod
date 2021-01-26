f43987473373e8ba7697b03a2949d3efb2853d6f
Cataldo Azzariti <cataldo.azzariti@sapient.com>
2021-01-26 21:50:12 +0000

    feat: works returning json

--- src/gcp/functions/tempreadings/go.mod
+++ src/gcp/functions/tempreadings/go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	cloud.google.com/go v0.57.0
 	cloud.google.com/go/bigquery v1.8.0
+	google.golang.org/api v0.24.0
 )
--- src/gcp/functions/tempreadings/tempreadings.go
+++ src/gcp/functions/tempreadings/tempreadings.go
@@ -1,12 +1,14 @@
 package tempreadings
 
 import (
-	"fmt"
-	// "context"
-	// "log"
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
 	"time"
-	// "cloud.google.com/go/bigquery"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
 )
 
 // TODO: Put in shared module
@@ -14,30 +16,45 @@ type bqParsedTempTableRow struct {
 	PubSubMessageID string    `bigquery:"pubsub_message_id"`
 	ProcessingTime  time.Time `bigquery:"processing_time"`
 	DeviceMessageID string    `json:"device_message_id" bigquery:"device_message_id"`
-	Temperature     string    `json:"temperature" bigquery:"temperature"`
-	Humidity        string    `json:"humidity" bigquery:"humidity"`
-	MeasurementTime string    `json:"measurement_time" bigquery:"measurement_time"`
+	Temperature     float32   `json:"temperature" bigquery:"temperature"`
+	Humidity        float32   `json:"humidity" bigquery:"humidity"`
+	MeasurementTime time.Time `json:"measurement_time" bigquery:"measurement_time"`
 }
 
 func RetrieveTempreadings(w http.ResponseWriter, r *http.Request) {
-	// ctx := context.Background()
+	ctx := context.Background()
 
-	// // TODO: REMOVE HARDCODING AND PUT ENV VAR
-	// client, err := bigquery.NewClient(ctx, "temp-measure-dev")
-	// if err != nil {
-	// 	log.Fatalf("Error initialising client %v", err)
-	// }
-	// defer client.Close()
+	// TODO: REMOVE HARDCODING AND PUT ENV VAR
+	client, err := bigquery.NewClient(ctx, "temp-measure-dev")
+	if err != nil {
+		log.Fatalf("Error initialising client %v", err)
+	}
+	defer client.Close()
 
-	// // TODO: REMOVE HARDCODING AND PUT ENV VAR
-	// row, err := client.Query(
-	// 	`SELECT * FROM ` + "`temp-measure-dev.temp_measure.temp_history_parsed`" + ` WHERE DATE(processing_time) = DATE(CURRENT_DATETIME())
-	// 	order by measurement_time desc
-	// 	LIMIT 100`).Read(ctx)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// TODO: REMOVE HARDCODING AND PUT ENV VAR
+	rows, err := client.Query(
+		`SELECT * FROM ` + "`temp-measure-dev.temp_measure.temp_history_parsed`" + ` WHERE DATE(processing_time) = DATE(CURRENT_DATETIME())
+		order by measurement_time desc
+		LIMIT 100`).Read(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Fprint(w, "hola chicaa")
+	// Initialise container
+	tempMeasurements := make([]bqParsedTempTableRow, 0)
 
+	for {
+		var singleRow bqParsedTempTableRow
+		err := rows.Next(&singleRow)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error in parsing row: %v; %v", singleRow, err)
+		}
+		tempMeasurements = append(tempMeasurements, singleRow)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	// fmt.Fprintf(w, tempMeasurements)
+	json.NewEncoder(w).Encode(tempMeasurements)
 }
